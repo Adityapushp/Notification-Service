c987baf8041f55e9c77089178ef0c73173d48504
Adityapushp <adityapushp.123@gmail.com>
2025-05-19 03:00:32 +0530

    retryHandler.js

---
+++ utils/retryHandler.js
@@ -0,0 +1,9 @@
+exports.retry = async(fn, retries = 3) => {
+    for (let i = 0; i < retries; i++) {
+        try {
+            return await fn();
+        } catch (e) {
+            if (i === retries - 1) throw e;
+        }
+    }
+};
